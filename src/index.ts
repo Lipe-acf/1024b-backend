54709c548380155da4abbf064522bf9b9a66f30d
Lipe-acf <felpznav2020@gmail.com>
2026-03-03 12:46:36 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -17,7 +17,36 @@ console.log("O valor de x é " + f3.toString())
 // somaVetor([1,2,3]) => 6 
 // somaVetor([1,2,3,4]) => 10 
 
-function somaVetor(vetor:number[]) {
 
+
+function somaVetor(numeros:number[]):number{
+    let soma = 0
+for (let i = 0; i < vetor.length; i++) {
+   const element = vetor[i]
+
+    soma = soma + element!
+}
+return soma
 
 }
+
+console.log(`O valor é ${somaVetor([1,2,3,4])}`)
+
+// Some as posições de um vetor que são pares
+
+function somaPares(numeros:number[]):number{
+    let soma = 0
+    for (let i = 0; i < vetor.length; i++) {
+        const element = vetor[i] 
+        if ( % 2 ==0) {
+            soma = soma + element!
+        }
+    }
+    return soma
+}
+
+console.log(`O valor é: ${somaPares([1,2,3,4])}`)
+
+
+//faça uma função que receba 2 vetores e faça a operação de merge entre eles
+//ex: mergeVetor([1,2,3],[4,5,6]) => [1,2,3,4,5,6]
