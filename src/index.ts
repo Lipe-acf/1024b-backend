a408bb37c870c201def2a0a5c667d6762dbc12ed
Lipe-acf <felpznav2020@gmail.com>
2026-03-03 12:18:54 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -13,4 +13,11 @@ const f = (variavel:number):number=> 10
 const f3 = f(10)
 console.log("O valor de x é " + f3.toString())
 
+//Terminem essa função para retornar a soma de todos os elementos de um vetor 
+// somaVetor([1,2,3]) => 6 
+// somaVetor([1,2,3,4]) => 10 
 
+function somaVetor(vetor:number[]) {
+
+
+}
