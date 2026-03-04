2d1c390de22858d08b90415c0fb1ccc2c70e1859
Lipe-acf <felpznav2020@gmail.com>
2026-03-04 11:19:30 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -62,4 +62,50 @@ console.log(juntaVetor([1,2,3], [4,5,6]))
 
 
 
-//
+/**
+ * Exercício 01 - cria um novo vetor com os valores do vetor original mais dois novos valores
+ * Nome da função - criaNovoVetor
+ * Crie uma função que retorne um novo vetor com os valores do vetor original mais dois novos valores
+ * criaNovoVetor([1, 2, 3], 4, 5) // [1, 2, 3, 4, 5]
+ * criaNovoVetor([1, 2, 3], 0, 0) // [1, 2, 3, 0, 0]
+ */ 
+
+//Início do seu código
+
+function criaNovoVetor(vetorC:number[], valor1:number, valor2:number) {
+    let novoVetor = vetorC.concat(valor1, valor2)
+    return novoVetor
+}
+
+console.log(criaNovoVetor([1,2,3], 0, 0))
+
+//Fim do seu código
+
+/**
+ * Exercício 02 - divisivelPor11
+ * Nome da função - divisivelPor11
+ * Crie uma função que retorna um array com os números divisíveis por 11 no intervalo
+ * @param {number} min Número mínimo
+ * @param {number} max Número máximo
+ * @returns {number[]} Retorna um array com os números divisíveis por 11 no intervalo
+ * @example
+ *  divisivelPor11(1, 100) // [11, 22, 33, 44, 55, 66, 77, 88, 99]
+ *  
+ * divisivelPor11(11, 110) // [11, 22, 33, 44, 55, 66, 77, 88, 99, 110]
+ */
+
+//Início do seu código
+function divisivelPor11(min:number, max:number) {
+    for (let i = min; i <= max; i++) {
+        let resultado:number[] = []
+    
+        if (i % 11 == 0) {
+            resultado.push(i)
+        }
+        return resultado
+    }
+}
+
+console.log(divisivelPor11(1,101))
+
+//Fim do seu código
