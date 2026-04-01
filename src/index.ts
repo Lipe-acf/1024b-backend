753149d04e92dc9ce5cac61eeb9dd1efddffdbc3
Lipe-acf <felpznav2020@gmail.com>
2026-04-01 09:34:51 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -9,45 +9,77 @@ const connection = mysql.createPool({
   database: 'aula1',
 });
 
-app.get("/pessoa", async (req, res)=>{
-  try { 
+app.get("/pessoa", async (req, res) => {
+  try {
 
-  const nome = "Algum nome"
-  const [resultado, campos] 
+    const nome = "Algum nome"
+    const [resultado, campos]
       = await connection.execute(`SELECT * FROM pessoa`)
- console.log(resultado)
- res.status(200).json(resultado)
-} catch (err) {
-  console.log(err);
-  res.status(500).json({mensagem:"Erro no servidor!"})
-}
+    console.log(resultado)
+    res.status(200).json(resultado)
+  } catch (err) {
+    console.log(err);
+
+    if (err instanceof Error && 'code' in err && err.code === 'ECONNREFUSED') {
+      res.status(500).json({ mensagem: "Erro: Ligue o LARAGON!!!" })
+    }
+    else if (err instanceof Error && 'code' in err && err.code === 'ER_BAD_DB_ERROR') {
+      res.status(500).json({ mensagem: "Erro: Crie o banco de dados ou confira se o nome está correto!" })
+    } else if (err instanceof Error && 'code' in err && err.code === 'ER_ACCESS_DENIED_ERROR') {
+      res.status(500).json({ mensagem: "Erro: Confira o seu USER e Senha de Conexão!" })
+    } else if (err instanceof Error && 'code' in err && err.code === 'ER_NO_SUCH_TABLE') {
+      res.status(500).json({ mensagem: "Erro: Confira o nome de sua tabela ou crie a tabela!" })
+    } else if (err instanceof Error && 'code' in err && err.code === 'ER_PARSE_ERROR') {
+      res.status(500).json({ mensagem: "Erro: Confira o código SQL do EXECUTE!" })
+    } else {
+
+
+      res.status(500).json({ mensagem: "Erro no servidor!" })
+    }
+  }
 
 }) //listar
-app.post("/pessoa", async (req, res)=>{
-  try { 
-  // "execute" irá chamar internamente a preparação e a consulta (query)
-  // const preparacao = await connection.prepare("select * from pessoa");
-  const {id, nome} = req.body
-//Valide se o id e o nome foram passados corretamente. (Algum valor)
-//Se não foram, retone o código 400 com a mensagem "id ou nome inválidos"
-//Não deixe o código executar a parte de baixo quando for inválido.
+app.post("/pessoa", async (req, res) => {
+  try {
+    // "execute" irá chamar internamente a preparação e a consulta (query)
+    // const preparacao = await connection.prepare("select * from pessoa");
+    const { id, nome } = req.body
+    //Valide se o id e o nome foram passados corretamente. (Algum valor)
+    //Se não foram, retone o código 400 com a mensagem "id ou nome inválidos"
+    //Não deixe o código executar a parte de baixo quando for inválido.
 
-if (id != null && nome != "") {
-  res.status(400).json({mensagem:"Id ou nome diferente de 0"})
-} 
+    if (id != null && nome != "") {
+      res.status(400).json({ mensagem: "Id ou nome diferente de 0" })
+    }
 
-  const [resultado, campos] 
-      = await connection.execute(`insert into pessoa value (?,?)`,[id,nome])
-  res.status(201).json({mesagem:"Sucesso"})
-  console.log(resultado)
-} catch (err) {
-  console.log(err);
-  res.status(500).json({mensagem:"Erro no servidor!"})
-}
+    const [resultado, campos]
+      = await connection.execute(`insert into pessoa value (?,?)`, [id, nome])
+    res.status(201).json({ mesagem: "Sucesso" })
+    console.log(resultado)
+  } catch (err) {
+    console.log(err);
+
+    if (err instanceof Error && 'code' in err && err.code === 'ECONNREFUSED') {
+      res.status(500).json({ mensagem: "Erro: Ligue o LARAGON!!!" })
+    }
+    else if (err instanceof Error && 'code' in err && err.code === 'ER_BAD_DB_ERROR') {
+      res.status(500).json({ mensagem: "Erro: Crie o banco de dados ou confira se o nome está correto!" })
+    } else if (err instanceof Error && 'code' in err && err.code === 'ER_ACCESS_DENIED_ERROR') {
+      res.status(500).json({ mensagem: "Erro: Confira o seu USER e Senha de Conexão!" })
+    } else if (err instanceof Error && 'code' in err && err.code === 'ER_NO_SUCH_TABLE') {
+      res.status(500).json({ mensagem: "Erro: Confira o nome de sua tabela ou crie a tabela!" })
+    } else if (err instanceof Error && 'code' in err && err.code === 'ER_PARSE_ERROR') {
+      res.status(500).json({ mensagem: "Erro: Confira o código SQL do EXECUTE!" })
+    } else {
+
+
+      res.status(500).json({ mensagem: "Erro no servidor!" })
+    }
+  }
 }) //inserir
 
 //Criar servidor
-app.listen(8000,()=>{
+app.listen(8000, () => {
   console.log("Servidor iniciado na porta 8000")
 })
 
